a9cf45b8a75fda757d3f0468d199a43396f4ef72
Jayson Reyes <marklawrencezalamea@gmail.com>
2022-10-30 18:54:20 +0800

    commit

--- Hackathon/sec_dashboard.php
+++ Hackathon/sec_dashboard.php
@@ -1,13 +1,24 @@
+
+<?php
+
+session_start();
+
+require("db_connect.php");
+?>
+
 <!DOCTYPE html>
 <!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
     <!--<title> Drop Down Sidebar Menu | CodingLab </title>-->
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="admin.css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <!-- Boxiocns CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="https://www.clipartkey.com/mpngs/m/228-2282541_isabela-cauayan-city-circle.png" />
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    
+    
    </head>
 <body>
   <div class="sidebar close">
@@ -119,10 +130,7 @@
       <div class="profile-content">
         <!--<img src="image/profile.jpg" alt="profileImg">-->
       </div>
-      <div class="name-job">
-        <div class="profile_name">Prem Shahi</div>
-        <div class="job">Web Desginer</div>
-      </div>
+    <h3>Log Out</h3>
       <i class='bx bx-log-out' ></i>
     </div>
   </li>
@@ -133,7 +141,92 @@
       <i class='bx bx-menu' ></i>
       <span class="text">Barangay Secretary Dashboard</span>
     </div>
+    <div class ="text-right">
+<a href="oneway.php" class="btn btn-md btn-primary" role="button" data-bs-toggle="button" aria-pressed="true">Add Record</a>
+
+	<hr><table class="table table-success table-striped">
+	
+  <thead align="center">
+    <tr >
+      <th scope="col">#</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Mobile Number</th>
+      <th scope="col">To</th>
+      <th scope="col">From</th>
+      <th scope="col">Departure Date</th>
+      <th scope="col">Class</th>
+      <th scope="col">About</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  
+  </tbody>
+
+
+<?php
+	$sql = "SELECT 
+			id, 
+			first, 
+			last, 
+			email,
+			mobile, 
+			area,
+			country,
+			departure,
+			ticket,
+			about
+			FROM oneway";
+	
+	$result = $conn->prepare($sql);
+	$result->execute();
+	
+	$count = $result->rowCount();
+	
+	if($result->rowCount()>0){
+		
+		$i=1;
+		while ($row = $result->fetch(PDO::FETCH_ASSOC))
+		{
+			echo  "<tr>
+						<td>" . $i . "</td>
+						<td>" . $row['first'] . "</td>
+						<td>" . $row['last'] . "</td>
+						<td>" . $row['email'] . "</td>
+						<td>" . $row['mobile'] . "</td>
+						<td>" . $row['area'] . "</td>
+						<td>" . $row['country'] . "</td>
+						<td>" . $row['departure'] . "</td>
+						<td>" . $row['ticket'] . "</td>
+						<td>" . $row['about'] . "</td>
+		<td>
+		<a class = 'btn btn-warning' href = 'oneway_edit.php?id=" . $row['id'] . "'>Edit</a> | ";
+	?>	
+		<a class = 'btn btn-warning' href = 'deleting.php?id=<?php echo $row['id'];?>' onClick = "return confirm('Are you sure want to delete this record?')">Delete</a></td>
+	<?php					
+		echo  "</tr>";
+		$i++;
+		}
+		echo "<tr><td colspan = '3'>Number of records: $count</td></tr>";
+		
+	}else
+	{
+		echo "<tr><td = colspan = '5'>No records found!</td></tr>";
+	}
+
+	
+?>
+	</tbody>
+	
+</table><hr>
+
+</body>
   </section>
+
+ 
+
+
   <script>
   let arrow = document.querySelectorAll(".arrow");
   for (var i = 0; i < arrow.length; i++) {
